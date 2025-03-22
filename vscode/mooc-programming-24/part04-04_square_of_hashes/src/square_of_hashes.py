64ce28fa2915b4860f3dfcc786a9b50dc7bcecdb
Anthony Mack <mackattack713@gmail.com>
2025-03-22 15:53:15 -0500

    Initial commit for Helsinki Python 2024 coursework

--- vscode/mooc-programming-24/part04-04_square_of_hashes/src/square_of_hashes.py
+++ vscode/mooc-programming-24/part04-04_square_of_hashes/src/square_of_hashes.py
@@ -1,8 +1,16 @@
 # Copy here code of line function from previous exercise
+def line(num, text):
+    if text == "":
+        print("*"*num)    
+    elif len(text) >= 1:
+        print(num*text[0])
 
 def square_of_hashes(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    count = 0
+    while count < size:
+        line(size, "#")
+        count += 1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
--- vscode/mooc-programming-24/part04-05_square/src/square.py
+++ vscode/mooc-programming-24/part04-05_square/src/square.py
@@ -1,8 +1,16 @@
 # Copy here code of line function from previous exercise
+def line(num, text):
+    if text == "":
+        print("*"*num)    
+    elif len(text) >= 1:
+        print(num*text[0])
 
 def square(size, character):
     # You should call function line here with proper parameters
-    line(4, "x")
+    count = 0
+    while count < size:
+        line(size, character)
+        count += 1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
