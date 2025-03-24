058cdba92c5a52410ecfbf7ac63319d85db643cc
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:08:26 -0500

    Update everything_reversed.py

--- vscode/mooc-programming-24/part04-33_everything_reversed/src/everything_reversed.py
+++ vscode/mooc-programming-24/part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def everything_reversed(my_list: list):
+    new_list = []
+    for x in my_list[::-1]:
+        new_list.append(x[::-1])
+    return(new_list)
+
+if __name__ == "__main__":
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
