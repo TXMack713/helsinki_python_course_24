2882a3f5fc4d07bef77a9a53730cc593bec2bcce
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:11:17 -0500

    Update sum_of_lists.py

--- vscode/mooc-programming-24/part04-27_sum_of_lists/src/sum_of_lists.py
+++ vscode/mooc-programming-24/part04-27_sum_of_lists/src/sum_of_lists.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def list_sum(nums1, nums2):
+    sum = []
+    count = len(nums1)
+    counter = 0
+    for x in nums1:
+        total = x + nums2[counter]
+        sum.append(total)
+        counter += 1
+    return sum
+
+if __name__ == "__main__":
+    a = [1, 2, 3]
+    b = [7, 8, 9]
+    print(list_sum(a, b)) # [8, 10, 12]
