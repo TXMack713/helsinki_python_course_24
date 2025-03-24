4e6db25d9e2ac7a8ff23f0df0072d10a9e4745c1
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:06:57 -0500

    Update no_shouting_allowed.py

--- vscode/mooc-programming-24/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ vscode/mooc-programming-24/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def no_shouting(string_list: list):
+    new_list = []
+    for x in string_list:
+        if x.isupper() == True:
+            continue
+        else:
+            new_list.append(x)
+    return new_list
+
+if __name__ == "__main__":
+    my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+    pruned_list = no_shouting(my_list)
+    print(pruned_list)
