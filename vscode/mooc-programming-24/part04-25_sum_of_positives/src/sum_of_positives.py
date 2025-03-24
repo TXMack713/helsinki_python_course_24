9eb72a85fb5d8fd2e85b89667b7d7d36f4e1331d
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:12:04 -0500

    Update sum_of_positives.py

--- vscode/mooc-programming-24/part04-25_sum_of_positives/src/sum_of_positives.py
+++ vscode/mooc-programming-24/part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def sum_of_positives(int_list):
+    sum = 0
+    for x in int_list:
+        if x > 0:
+            sum += x
+    return sum
+
+if __name__ == "__main__":
+    my_list = [1, -2, 3, -4, 5]
+    result = sum_of_positives(my_list)
+    print("The result is", result)
