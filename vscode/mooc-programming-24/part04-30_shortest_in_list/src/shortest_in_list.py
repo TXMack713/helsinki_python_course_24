4ce510dbb56409e1ca466a7fb706e8059f0dd7a7
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:09:49 -0500

    Update shortest_in_list.py

--- vscode/mooc-programming-24/part04-30_shortest_in_list/src/shortest_in_list.py
+++ vscode/mooc-programming-24/part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,19 @@
-# Write your solution here
+def shortest(words):
+    shortest = 0
+    shortest_word = ""
+    for x in words:
+        if shortest == 0:
+            shortest = len(x)
+            shortest_word = x
+        elif len(x) <= shortest:
+            shortest_word = x
+            shortest = len(x)
+    return shortest_word
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = shortest(my_list)
+    print(result)
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = shortest(my_list)
+    print(result)
