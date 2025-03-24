9e7789da768c17810a17ee0d08b12511bbffd09b
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:08:55 -0500

    Update integers_to_strings.py

--- vscode/mooc-programming-24/part04-32_integers_to_strings/src/integers_to_strings.py
+++ vscode/mooc-programming-24/part04-32_integers_to_strings/src/integers_to_strings.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def formatted(the_list):
+    new_list = []
+    for x in the_list:
+        change = f"{x:.2f}"
+        new_list.append(change)
+    return new_list
+
+if __name__ == "__main__":
+    my_list = [1.234, 0.3333, 0.11111, 3.446]
+    new_list = formatted(my_list)
+    print(new_list)
