f0f4e44f88b522690f7bffd4af81b3867e475404
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:10:30 -0500

    Update length_of_longest.py

--- vscode/mooc-programming-24/part04-29_length_of_longest/src/length_of_longest.py
+++ vscode/mooc-programming-24/part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def length_of_longest(words):
+    longest = 0
+    max_word = ""
+    for x in words:
+        if len(x) > longest:
+            max_word = x
+            longest = len(x)
+    return longest
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = length_of_longest(my_list)
+    print(result)
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = length_of_longest(my_list)
+    print(result)
