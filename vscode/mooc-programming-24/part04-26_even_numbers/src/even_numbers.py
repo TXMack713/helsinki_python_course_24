7cb3dd6bc534b65281dfd42866c0e3be96e8b606
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:11:41 -0500

    Update even_numbers.py

--- vscode/mooc-programming-24/part04-26_even_numbers/src/even_numbers.py
+++ vscode/mooc-programming-24/part04-26_even_numbers/src/even_numbers.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def even_numbers(nums):
+    evens = []
+    for x in nums:
+        if x % 2 == 0:
+            evens.append(x)
+    return evens
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    new_list = even_numbers(my_list)
+    print("original", my_list)
+    print("new", new_list)
