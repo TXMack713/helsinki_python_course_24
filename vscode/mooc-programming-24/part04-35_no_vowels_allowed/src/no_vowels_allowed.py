598231441804393ecb48d533d8ff45f559f9916b
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:07:31 -0500

    Update no_vowels_allowed.py

--- vscode/mooc-programming-24/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ vscode/mooc-programming-24/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def no_vowels(string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    new_string = string
+    for x in vowels:
+        new_string = new_string.replace(x, "")
+    return new_string
+
+if __name__ == "__main__":
+    my_string = "this is an example"
+    print(no_vowels(my_string))
