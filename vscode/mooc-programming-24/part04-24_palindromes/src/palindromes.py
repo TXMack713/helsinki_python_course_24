7308cf0d8b4c930af0611a36f60d838109e7d9f1
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 09:41:25 -0500

    Update palindromes.py
    Solved the palindromes problem.

--- vscode/mooc-programming-24/part04-24_palindromes/src/palindromes.py
+++ vscode/mooc-programming-24/part04-24_palindromes/src/palindromes.py
@@ -1,4 +1,25 @@
 # Write your solution here
-# Note, that at this time the main program should not be written inside
+def palindromes(words):
+    list1 = list(words.lower())
+    list2 = list(words.lower())
+    list2.reverse()
+    while True:
+        if list1 == list2:
+            print(words + " is a palindrome!")
+            return True
+        else:
+            print("that wasn't a palindrome")
+            return False
+            
+# # Note, that at this time the main program should not be written inside
 # if __name__ == "__main__":
-# block!
+result = False
+while result == False:
+    string = input("Please type in a palindrome: ")
+    result = palindromes(string)
+    if result == True:
+        # print(string + " is a palindrome!")
+        break
+    else:
+        # print("that wasn't a palindrome")
+        continue
