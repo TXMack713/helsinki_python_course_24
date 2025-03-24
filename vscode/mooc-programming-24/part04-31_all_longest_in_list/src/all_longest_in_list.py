4f7ac5663fe4b24b2e91efd37af1a46b02592a99
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:09:19 -0500

    Update all_longest_in_list.py

--- vscode/mooc-programming-24/part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ vscode/mooc-programming-24/part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,1 +1,21 @@
 # Write your solution here
+def all_the_longest(words):
+    temp = []
+    length = 0
+    for x in words:
+        if len(x) >= length:
+            temp.append(x)
+            length = len(x)
+    final = []
+    for x in temp:
+        if len(x) >= length:
+            final.append(x)
+    return final
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = all_the_longest(my_list)
+    print(result) # ['eleventh']
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = all_the_longest(my_list)
+    print(result) # ['dorothy', 'richard']
