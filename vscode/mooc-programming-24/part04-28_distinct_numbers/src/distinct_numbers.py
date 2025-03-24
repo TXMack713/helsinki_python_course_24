c0d8cca26b1172fdb9dd3f1dd9f38bb213a6633d
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:10:54 -0500

    Update distinct_numbers.py

--- vscode/mooc-programming-24/part04-28_distinct_numbers/src/distinct_numbers.py
+++ vscode/mooc-programming-24/part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def distinct_numbers(nums):
+    unique = []
+    for x in nums:
+        if unique.count(x) == 0:
+            unique.append(x)
+    unique.sort()
+    return unique
+
+if __name__ == "__main__":
+    my_list = [3, 2, 2, 1, 3, 3, 1]
+    print(distinct_numbers(my_list)) # [1, 2, 3]
