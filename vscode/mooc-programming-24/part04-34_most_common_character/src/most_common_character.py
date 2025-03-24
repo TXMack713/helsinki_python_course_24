34ac8500f9e3931cb4c7b40f157beaabe35a4807
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 13:08:02 -0500

    Update most_common_character.py

--- vscode/mooc-programming-24/part04-34_most_common_character/src/most_common_character.py
+++ vscode/mooc-programming-24/part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def most_common_character(string):
+    count = 0
+    char = ""
+    for x in string:
+        if string.count(x) > count:
+            count = string.count(x)
+            char = x
+    return char
+
+if __name__ == "__main__":
+    first_string = "abcdbde"
+    print(most_common_character(first_string))
+
+    second_string = "exemplaryelementary"
+    print(most_common_character(second_string))
