c4a094d6bf50c346feba9ccd22e498341894966e
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-03-24 19:25:47 -0500

    Update grade_statistics.py

--- vscode/mooc-programming-24/part04-38_grade_statistics/src/grade_statistics.py
+++ vscode/mooc-programming-24/part04-38_grade_statistics/src/grade_statistics.py
@@ -1,1 +1,23 @@
 # Write your solution here
+def gather_data(grades: list):
+    exam_points = []
+    exercises_completed = []
+    response = input("Exam points and exercises completed: ")
+    if response == "":
+        break
+    else:
+        response_list = response.split(" ")
+        exam_points.append(int(response_list[0]))
+        exercises_completed.append(int(response_list[1]))
+    all_grades = [exam_points, exercises_completed]
+    return all_grades
+
+def exercise_points(exercises: list):
+    points = []
+    for x in exercises:
+        point = x/100
+        points.append(point)
+
+def main():
+
+
