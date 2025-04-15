0c259187f4e84ec027e90c067e14958b145518a8
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-04-14 21:12:22 -0500

    Update diary.py

--- vscode/mooc-programming-24/part06-11_diary/src/diary.py
+++ vscode/mooc-programming-24/part06-11_diary/src/diary.py
@@ -1,1 +1,34 @@
 # Write your solution here
+def diary():
+    file = "diary.txt"
+    print("1 - add an entry, 2 - read entries, 0 - quit")
+    decision = int(input("Function: "))
+    while decision != 0:
+        if decision == 1:
+            entry = input("Diary entry: ")
+            with open(file, "a") as diary_file:
+                diary_file.write(entry + "\n")
+            print("Diary saved")
+            print("")
+            print("1 - add an entry, 2 - read entries, 0 - quit")
+            decision = int(input("Function: "))
+        elif decision == 2:
+            with open(file) as diary_file:
+                print("Entries:")
+                for line in diary_file:
+                    print(line)
+            print("1 - add an entry, 2 - read entries, 0 - quit")
+            decision = int(input("Function: "))
+        elif decision != 0 and decision != 1 and decision != 2:
+            print("Invalid entry")
+            print("1 - add an entry, 2 - read entries, 0 - quit")
+            decision = int(input("Function: "))
+        # else:
+        #     print("Bye now!")
+        #     break
+    if decision == 0:
+        print("Bye now!")
+
+diary()
+# if __name__ == "__main__":
+#     diary()
