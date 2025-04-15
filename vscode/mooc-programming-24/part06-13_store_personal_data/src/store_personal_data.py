946408ca942aa69bb61d00d57ca13ba139af2639
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-04-14 21:11:27 -0500

    Update store_personal_data.py

--- vscode/mooc-programming-24/part06-13_store_personal_data/src/store_personal_data.py
+++ vscode/mooc-programming-24/part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,8 @@
 # Write your solution here
+def store_personal_data(person: tuple):
+    with open("people.csv", "a") as file:
+        string = f"{person[0]};{person[1]};{person[2]}" + "\n"
+        file.write(string)
+    
+if __name__ == "__main__":
+    store_personal_data()
