d0d2c7364eb79af3d8cda5aa14118c030a6c48d4
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-04-30 08:42:42 -0500

    Completed the dictionary file challenge.

--- vscode/tmcdata/mooc-programming-24/part06-16_dictionary_file/src/dictionary_file.py
+++ vscode/tmcdata/mooc-programming-24/part06-16_dictionary_file/src/dictionary_file.py
@@ -1,33 +1,55 @@
 # Write your solution here
 word_dictionary = {}
-with open("dictionary.txt") as file:
-    for line in file:
-        array = []
-        array = line.split(" ")
-        word_dictionary[array[0]] = array[1]
+added_words = {}
+def read_dictionary():    
+    with open("dictionary.txt") as file:
+        for line in file:
+            array = []
+            line = line.strip()
+            array = line.split("-")
+            if len(array) > 1:
+                word_dictionary[array[0].strip()] = array[1].strip()
+            else:
+                continue
+    # return word_dictionary
         
 def dictionary():
+    read_dictionary()
     answer = True
     while answer:
         response = prompt()
         if response == 3:
             answer = False
             print("Bye!")
+            with open("dictionary.txt", "a") as file:
+                for key, value in added_words.items():
+                    file.write(key + " - " + value + "\n")
             break
         elif response == 2:
             term = input("Search term: ")
-            for key, value in word_dictionary.items():
+            search_dictionary = {}
+            with open("dictionary.txt") as file:
+                for line in file:
+                    array = []
+                    line = line.strip()
+                    array = line.split("-")
+                    if len(array) > 1:
+                        search_dictionary[array[0].strip()] = array[1].strip()
+                    else:
+                        continue
+            for key, value in search_dictionary.items():
                 if key.find(term) != -1 or value.find(term) != -1:
                     print(key + " - " + value)
         elif response == 1:
             finn = input("The word in Finnish: ")
             eng = input("The word in English: ")
-            word_dictionary[finn] = eng
+            for key, value in word_dictionary.items():
+                added_words[finn] = eng
             print("Dictionary entry added")
             # response = prompt()
-    with open("dictionary.txt", "w") as file:
-        for key, value in word_dictionary.items():
-            file.write(key + " - " + value)
+            with open("dictionary.txt", "a") as file:
+                # for key, value in word_dictionary.items():
+                file.write(finn + " - " + eng + "\n")
 
 def prompt():
     print("1 - Add word, 2 - Search, 3 - Quit")
