a0a9555afb5f173b37f0a40b39b69e0e710d46e5
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-05-05 12:17:46 -0500

    Update read_input.py

--- vscode/tmcdata/mooc-programming-24/part06-17_read_input/src/read_input.py
+++ vscode/tmcdata/mooc-programming-24/part06-17_read_input/src/read_input.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def read_input(text: str, lower_boundary: int, upper_boundary: int):
+    
+    while True:
+        try:
+            entry = input(text)
+            if int(entry) >= lower_boundary and int(entry) <= upper_boundary:
+                return int(entry)
+        except ValueError:
+            pass
+        print(f"You must type in an integer between {lower_boundary} and {upper_boundary}")
+
+if __name__ == "__main__":
+    number = read_input("Please type in a number: ", 5, 10)
+    print("You typed in:", number)
