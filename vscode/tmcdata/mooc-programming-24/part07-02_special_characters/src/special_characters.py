4d136fa10c2fd1992e6d689f0bb1ca44b08a17a8
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-05-14 10:11:59 -0500

    Update special_characters.py

--- vscode/tmcdata/mooc-programming-24/part07-02_special_characters/src/special_characters.py
+++ vscode/tmcdata/mooc-programming-24/part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,21 @@
 # Write your solution here
+import string
+def separate_characters(my_string: str):
+    letters = ""
+    punctuations = ""
+    all_others = ""
+    for item in my_string:
+        if item in string.ascii_letters:
+            letters += item
+        elif item in string.punctuation:
+            punctuations += item
+        else:
+            all_others += item
+        
+    return(letters, punctuations, all_others)
+
+if __name__ == "__main__":
+    parts = separate_characters("Olé!!! Hey, are ümläüts wörking?")
+    print(parts[0])
+    print(parts[1])
+    print(parts[2])
