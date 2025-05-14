dec33236d93672021249fc8cb8e6ede7ecf49106
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-05-14 10:20:54 -0500

    Update lottery_numbers.py

--- vscode/tmcdata/mooc-programming-24/part07-04_lottery_numbers/src/lottery_numbers.py
+++ vscode/tmcdata/mooc-programming-24/part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,12 @@
 # Write your solution here
+import random
+
+def lottery_numbers(amount: int, lower: int, upper: int):
+    number_list = list(range(lower, upper+1))
+    lotto_numbers = random.sample(number_list, amount)
+    lotto_numbers.sort()
+    return lotto_numbers
+
+if __name__ == "__main__":
+    for number in lottery_numbers(7, 1, 40):
+        print(number)
