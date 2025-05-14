042ca523271fa261e6936212b355f46cfdd5700f
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-05-14 10:51:09 -0500

    Update password_generator_part_1.py

--- vscode/tmcdata/mooc-programming-24/part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ vscode/tmcdata/mooc-programming-24/part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,14 @@
 # Write your solution here
+import random
+import string
+def generate_password(length: int):
+    alpha_list = string.ascii_lowercase
+    letters = random.sample(alpha_list, length)
+    password = ""
+    for letter in letters:
+        password += letter
+    return password
+
+if __name__ == "__main__":
+    for i in range(10):
+        print(generate_password(8))
