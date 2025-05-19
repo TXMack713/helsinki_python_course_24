acf490cd4fb7a610f76076d140404dc8d5b54688
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-05-19 10:17:06 -0500

    Update random_words.py

--- vscode/tmcdata/mooc-programming-24/part07-08_random_words/src/random_words.py
+++ vscode/tmcdata/mooc-programming-24/part07-08_random_words/src/random_words.py
@@ -1,1 +1,29 @@
 # Write your solution here
+import random
+def words(n: int, beginning: str):
+    all_words = []
+    string_words = []
+    with open("words.txt") as file:
+        for line in file:
+            entry = line.strip("\n")
+            all_words.append(entry)
+        all_words.sort()
+        all_words.reverse()
+    
+    for word in all_words:
+        if word.startswith(beginning):
+            string_words.append(word)
+    
+    # counter = 2 * n
+    sample_words = random.sample(string_words, n)
+    if len(sample_words) < n:
+        raise ValueError
+    
+    found_words = random.sample(sample_words, n)
+       
+    return found_words
+
+if __name__ == "__main__":
+    word_list = words(3, "hi")
+    for word in word_list:
+        print(word)
