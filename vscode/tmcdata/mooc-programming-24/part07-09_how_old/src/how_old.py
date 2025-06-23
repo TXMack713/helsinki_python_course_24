1b26be666b5ad001c310c0122a90d82d8adc219e
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-06-23 17:45:53 -0500

    Update how_old.py

--- vscode/tmcdata/mooc-programming-24/part07-09_how_old/src/how_old.py
+++ vscode/tmcdata/mooc-programming-24/part07-09_how_old/src/how_old.py
@@ -2,32 +2,30 @@
 from datetime import datetime
 
 def get_date():
-    day = int(input("Day: "))
-    month = int(input("Month: "))
-    year = int(input("Year: "))
+    day = int(input("Day:"))
+    month = int(input("Month:"))
+    year = int(input("Year:"))
     # birth = datetime(year, month, day)
     return (day, month, year)
 
 def calculate_days():
     birthdate = get_date()
     birth = datetime(birthdate[2], birthdate[1], birthdate[0])
-    print(birth)
+
     # Calculate the millenium
     millenium = datetime(1999,12,31)
-    print(millenium)
 
     difference = millenium - birth
-    print(difference)
 
     result = difference.days
+    answer = ""
 
     if result > 0:
-        print("You were " + str(result) + " days old on the eve of the new millennium.")
+        answer = "You were " + str(result) + " days old on the eve of the new millennium."
     else:
-        print("You weren't born yet on the eve of the new millennium.")
+        answer = "You weren't born yet on the eve of the new millennium."
     
-    # return difference
+    return answer
 
-if __name__ == "__main__":
-    calculate_days()
+print(calculate_days())
 
