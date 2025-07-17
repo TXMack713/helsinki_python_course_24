5f7021b46239d345c48bc9d7df43cdf957b3cb6e
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-07-17 13:10:31 -0500

    Update course_statistics.py

--- vscode/tmcdata/mooc-programming-24/part07-13_course_statistics/src/course_statistics.py
+++ vscode/tmcdata/mooc-programming-24/part07-13_course_statistics/src/course_statistics.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+
+import urllib.request, json
+def retrieve_all():
+    my_request = urllib.request.urlopen("https://studies.cs.helsinki.fi/stats-mock/api/courses")
+
+    data = my_request.read()
+    course_info = json.loads(data)
+    # print(f"Course info: {course_info}")
+    list_of_courses = []
+
+    for entry in course_info:
+        # print(f"Current entry: {entry}")
+        if entry["enabled"] == True:
+            fullName = entry["fullName"]
+            name = entry["name"]
+            year = entry["year"]
+            sum = 0
+            for value in entry["exercises"]:
+                sum += value
+            list_of_courses.append((fullName, name, year, sum))
+    return list_of_courses
+
+def retrieve_course(course_name: str):
+    url = "https://studies.cs.helsinki.fi/stats-mock/api/courses/****/stats"
+    new_url = url.replace("****", course_name)
+    my_request = urllib.request.urlopen(url)
+
+print(retrieve_all())
+# print(my_request.read())
