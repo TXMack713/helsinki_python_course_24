662494cbe48209491f71ece89bf1df9717098798
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-08-28 13:07:39 -0500

    Completed course_statistics.py assignment

--- vscode/tmcdata/mooc-programming-24/part07-13_course_statistics/src/course_statistics.py
+++ vscode/tmcdata/mooc-programming-24/part07-13_course_statistics/src/course_statistics.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import urllib.request, json
+import urllib.request, json, math
 def retrieve_all():
     my_request = urllib.request.urlopen("https://studies.cs.helsinki.fi/stats-mock/api/courses")
 
@@ -22,9 +22,42 @@ def retrieve_all():
     return list_of_courses
 
 def retrieve_course(course_name: str):
+    # try:
     url = "https://studies.cs.helsinki.fi/stats-mock/api/courses/****/stats"
     new_url = url.replace("****", course_name)
-    my_request = urllib.request.urlopen(url)
+    my_request = urllib.request.urlopen(new_url)
+    data = my_request.read()
+    course_info = json.loads(data)
+    my_dict = {}
+    weeks = 0
+    students_max = 0
+    hours = 0
+    hours_average = 0
+    exercises = 0
+    exercises_average = 0
+    for keys, values in course_info.items():
+        weeks += 1
+        if course_info[keys]["students"] > students_max:
+            students_max = course_info[keys]["students"]
+        hours += course_info[keys]["hour_total"]
+        exercises += course_info[keys]["exercise_total"]
+    hours_average = math.floor(hours / students_max)
+    exercises_average = math.floor(exercises / students_max)
+    my_dict["weeks"] = weeks
+    my_dict["students"] = students_max
+    my_dict["hours"] = hours
+    my_dict["hours_average"] = hours_average
+    my_dict["exercises"] = exercises
+    my_dict["exercises_average"] = exercises_average    
+    
+    return my_dict
+    
+    # except:
+    #     print(f"An error occurred, there's no course named {course_name}")
+    # return None
 
-print(retrieve_all())
-# print(my_request.read())
+if __name__ == "__main__":
+    print(retrieve_all())
+    course = input("Please enter the course name:")
+    print(retrieve_course(course))
+    # print(my_request.read())
