2e7f86f0371044e633569d89db2f1b6d007ebc76
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-10-15 12:44:59 -0500

    Update spellchecker_2.py

--- vscode/tmcdata/mooc-programming-24/part07-16_spellchecker_2/src/spellchecker_2.py
+++ vscode/tmcdata/mooc-programming-24/part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import re
+import difflib
+
 def spell_checker():
     string = input("Please enter a text string of your choosing: ")
     words = string.split(" ")
@@ -10,10 +13,12 @@ def spell_checker():
     
     # Check for misspelled words
     new_words_list = []
+    bad_words_list = []
     for word in words:
         if word.lower() not in dictionary_list:
             word = "*" + word + "*"
             new_words_list.append(word)
+            bad_words_list.append(word)
             # print("Bad word", word)
         else:
             new_words_list.append(word)
@@ -23,7 +28,17 @@ def spell_checker():
     for word in new_words_list:
         output = output + " " + word
     
-    
     print(output.strip())
+    print("suggestions:")
+
+    for word in bad_words_list:
+        substitute_words = []
+        substitute_words = difflib.get_close_matches(word, dictionary_list, n=5, cutoff=0)
+        subs_output = ""
+        for entry in substitute_words:
+            subs_output = subs_output + entry + ", "
+        # print(substitute_words)
+        print(subs_output[:-2].strip())
+ 
 
 spell_checker()
