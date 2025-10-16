a225cd2637b31bcdebcfa998ee8b1db7247aff87
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-10-16 15:04:25 -0500

    Completed the string_helper exercise.

--- vscode/tmcdata/mooc-programming-24/part07-17_string_helper/src/string_helper.py
+++ vscode/tmcdata/mooc-programming-24/part07-17_string_helper/src/string_helper.py
@@ -1,1 +1,35 @@
-# Write your solution here
+#!/usr/bin/env python3
+
+def change_case(orig_string: str):
+    new_string = ""
+    for item in orig_string:
+        if item.islower():
+            item = item.upper()
+            new_string += item
+        elif item.isupper():
+            item = item.lower()
+            new_string += item
+        else:
+            new_string += item
+    return new_string
+
+def split_in_half(orig_string: str):
+    half = len(orig_string) // 2
+    first = orig_string[:half]
+    second = orig_string[half:]
+    final = (first, second)
+    return final
+
+def remove_special_characters(orig_string: str):
+    new_string = ""
+    for item in orig_string:
+        if item.isalnum() or item.isspace():
+            new_string += item
+        else:
+            continue
+    return new_string
+
+if __name__ == "__main__":
+    print(change_case("This is a test of the change case method!"))
+    print(split_in_half("First and second"))
+    print(remove_special_characters("This! is a test, of the change* case! method?"))
