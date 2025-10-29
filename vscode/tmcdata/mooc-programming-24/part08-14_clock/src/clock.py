9234830b550321068974da4d765ec65e04674950
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-10-29 10:25:36 -0500

    Completed the clock exercise.

--- vscode/tmcdata/mooc-programming-24/part08-14_clock/src/clock.py
+++ vscode/tmcdata/mooc-programming-24/part08-14_clock/src/clock.py
@@ -1,1 +1,51 @@
-# Write your solution here:
+#!/usr/bin/env python 3
+
+import time
+
+class Clock:
+    def __init__(self, hours: int, minutes: int, seconds: int):
+        self.seconds = seconds
+        self.minutes = minutes
+        self.hours = hours
+
+    def __str__(self):
+        return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
+    
+    def tick(self):
+        # time.sleep(1)
+        if self.seconds == 59:
+            self.seconds = 0
+            if self.minutes == 59:
+                self.minutes = 0
+                if self.hours == 23:
+                    self.hours = 0
+                else:
+                    self.hours += 1
+            else:
+                self.minutes += 1
+        else:
+            self.seconds += 1
+
+    def set(self, hour: int, minute: int):
+        self.seconds = 0
+        self.minutes = minute
+        self.hours = hour
+        
+if __name__ == "__main__":
+    clock = Clock(23, 59, 55)
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+
+    clock.set(12, 5)
+    print(clock)
