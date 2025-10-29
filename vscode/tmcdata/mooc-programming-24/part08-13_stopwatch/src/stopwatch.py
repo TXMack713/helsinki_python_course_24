79551bfb2c82cadc8f564845818172f72704c907
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-10-29 10:18:53 -0500

    Update stopwatch.py

--- vscode/tmcdata/mooc-programming-24/part08-13_stopwatch/src/stopwatch.py
+++ vscode/tmcdata/mooc-programming-24/part08-13_stopwatch/src/stopwatch.py
@@ -1,5 +1,28 @@
-# Write your solution here:
+#!/usr/bin/env python 3
+
+import time
+
 class Stopwatch:
     def __init__(self):
         self.seconds = 0
         self.minutes = 0
+
+    def __str__(self):
+        return f"{self.minutes:02}:{self.seconds:02}"
+    
+    def tick(self):
+        time.sleep(1)
+        if self.seconds == 59:
+            self.seconds = 0
+            if self.minutes == 59:
+                self.minutes = 0
+            else:
+                self.minutes += 1
+        else:
+            self.seconds += 1
+        
+if __name__ == "__main__":
+    watch = Stopwatch()
+    for i in range(12):
+        print(watch)
+        watch.tick()
