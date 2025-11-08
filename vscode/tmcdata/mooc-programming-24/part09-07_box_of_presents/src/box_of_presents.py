d4fae0116741a4cc01c07bc0ab65162254cb52eb
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-11-07 19:20:52 -0600

    Update box_of_presents.py

--- vscode/tmcdata/mooc-programming-24/part09-07_box_of_presents/src/box_of_presents.py
+++ vscode/tmcdata/mooc-programming-24/part09-07_box_of_presents/src/box_of_presents.py
@@ -1,1 +1,39 @@
-# WRITE YOUR SOLUTION HERE:
+#!/usr/bin/env python 3
+
+class Present:
+    def __init__(self, name: str, weight: int):
+        self.name = name
+        self.weight = weight
+
+    def __str__(self):
+        return f"{self.name} ({self.weight} kg)"
+
+class Box:
+    def __init__(self):
+        self.presents = []
+        self.weight = 0
+    
+    def add_present(self, present: Present):
+        self.presents.append(present)
+
+    def total_weight(self):
+        for item in self.presents:
+            self.weight += item.weight
+
+        return self.weight
+
+if __name__ == "__main__":
+    book = Present("ABC Book", 2)
+
+    print("The name of the present:", book.name)
+    print("The weight of the present:", book.weight)
+    print("Present", book)
+
+    box = Box()
+    box.add_present(book)
+    print(box.total_weight())
+
+    cd = Present("Pink Floy: Dark Side of the Moon", 1)
+    box.add_present(cd)
+    print(box.total_weight())
+    
