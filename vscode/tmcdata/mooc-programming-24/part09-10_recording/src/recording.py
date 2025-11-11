b5e129179644229c8dc8dd5fe251e1622537e124
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-11-11 12:13:19 -0600

    Completed the recording exercise.

--- vscode/tmcdata/mooc-programming-24/part09-10_recording/src/recording.py
+++ vscode/tmcdata/mooc-programming-24/part09-10_recording/src/recording.py
@@ -1,1 +1,28 @@
-# WRITE YOUR SOLUTION HERE:
+#!/usr/bin/env python 3
+
+class Recording:
+    def __init__(self, length: int):
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("The length of the recording cannnot be less than 0")
+
+    # getter method
+    @property
+    def length(self):
+        return self.__length
+    
+    # setter method
+    @length.setter
+    def length(self, length):
+        if length >= 0:
+            self.__length = length
+        else:
+            raise ValueError("The length of the recording cannnot be less than 0")
+        
+
+if __name__ == "__main__":
+    the_wall = Recording(43)
+    print(the_wall.length)
+    the_wall.length = 44
+    print(the_wall.length)
