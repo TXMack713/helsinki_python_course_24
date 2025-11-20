250c70bd13d9b569ce32cb2ac010d1e740751ffa
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-11-20 08:33:01 -0600

    Completed the game museum exercise.

--- vscode/tmcdata/mooc-programming-24/part10-02_game_museum/src/game_museum.py
+++ vscode/tmcdata/mooc-programming-24/part10-02_game_museum/src/game_museum.py
@@ -1,4 +1,5 @@
-# TEE RATKAISUSI TÄHÄN:
+#!/usr/bin/env python 3
+
 class ComputerGame:
     def __init__(self, name: str, publisher: str, year: int):
         self.name = name
@@ -15,3 +16,23 @@ class GameWarehouse:
     def list_games(self):
         return self.__games
 
+class GameMuseum(GameWarehouse):
+    def __init__(self):
+        super().__init__()
+        
+    def list_games(self):
+        self.__older_games = []
+        for game in super().list_games():
+            # print(f"{game.name}: {game.year}")
+            if game.year < 1990:
+                self.__older_games.append(game)
+        return self.__older_games
+
+if __name__ == "__main__":
+    museum = GameMuseum()
+    museum.add_game(ComputerGame("Pacman", "Namco", 1980))
+    museum.add_game(ComputerGame("GTA 2", "Rockstart", 1999))
+    museum.add_game(ComputerGame("Bubble Bobble", "Taito", 1986))
+    for game in museum.list_games():
+        print(game.name)
+    
