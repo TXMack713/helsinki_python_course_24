fd829a32b6a894a541ef76998ad1c9487b545b86
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-11-19 22:07:52 -0600

    Completed the laptop computer exercise

--- vscode/tmcdata/mooc-programming-24/part10-01_laptop_computer/src/laptop_computer.py
+++ vscode/tmcdata/mooc-programming-24/part10-01_laptop_computer/src/laptop_computer.py
@@ -1,4 +1,5 @@
-# Write your solution here:
+#!/usr/bin/env python 3
+
 class Computer:
     def __init__(self, model: str, speed: int):
         self.__model = model
@@ -11,3 +12,19 @@ class Computer:
     @property
     def speed(self):
         return self.__speed
+
+class LaptopComputer(Computer):
+    def __init__(self, model: str, speed: int, weight: int):
+        super().__init__(model, speed)
+        self.__weight = weight
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    def __str__(self):
+        return f"{self.model}, {self.speed} MHz, {self.weight} kg"
+
+if __name__ == "__main__":
+    laptop = LaptopComputer("NoteBook Pro15", 1500, 2)
+    print(laptop)
