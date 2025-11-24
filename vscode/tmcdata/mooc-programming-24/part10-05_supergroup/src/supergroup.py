e5b007719146ba10982431f4427d90455b9f468e
Anthony <49073356+TXMack713@users.noreply.github.com>
2025-11-24 09:38:09 -0600

    Completed the supergroup exercise.

--- vscode/tmcdata/mooc-programming-24/part10-05_supergroup/src/supergroup.py
+++ vscode/tmcdata/mooc-programming-24/part10-05_supergroup/src/supergroup.py
@@ -1,4 +1,5 @@
-# Write your solution here:
+#!/usr/bin/env python 3
+
 class SuperHero:
     def __init__(self, name: str, superpowers: str):
         self.name = name
@@ -6,3 +7,43 @@ class SuperHero:
 
     def __str__(self):
         return f'{self.name}, superpowers: {self.superpowers}'
+
+class SuperGroup:
+    def __init__(self, name: str, location: str):
+        self._name = name
+        self._location = location
+        self._members = []
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+    
+    @property
+    def location(self):
+        return self._location
+    
+    @location.setter
+    def location(self, location):
+        self._location = location
+    
+    def add_member(self, hero: SuperHero):
+        self._members.append(hero)
+    
+    def print_group(self):
+        print(f"{self._name}, {self._location}")
+        print("Members:")
+        for member in self._members:
+            print(f"{member.name}, superpowers: {member.superpowers}")
+
+if __name__ == "__main__":
+    superperson = SuperHero("SuperPerson", "Superspeed, superstrength")
+    invisible = SuperHero("Invisible Inca", "Invisibility")
+    revengers = SuperGroup("Revengers", "Emerald City")
+
+    revengers.add_member(superperson)
+    revengers.add_member(invisible)
+    revengers.print_group()
