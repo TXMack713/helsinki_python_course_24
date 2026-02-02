8a8995df6f12eb637da26828200a11e641c73e94
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-02 09:01:47 -0600

    Completed the begin_with_vowel exercise

--- vscode/tmcdata/mooc-programming-24/part11-06_begin_with_vowel/src/begin_with_vowel.py
+++ vscode/tmcdata/mooc-programming-24/part11-06_begin_with_vowel/src/begin_with_vowel.py
@@ -1,1 +1,11 @@
-# WRITE YOUR SOLUTION HERE:
+#!/usr/bin/env python 3
+
+def begin_with_vowel(words: list):
+    return [word for word in words if word.lower().startswith(("a","e","i","o","u"))]
+
+if __name__ == "__main__":
+    word_list = ["automobile", "motorbike", "Animal", "cat", 
+                 "Dog", "APPLE", "orange"]
+    for vowelled in begin_with_vowel(word_list):
+        print(vowelled)
+
