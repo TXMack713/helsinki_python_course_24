61844276140da00c4ddeb20e6e276ea15f71ba50
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-03 13:36:13 -0600

    Completed the filter_forbidden exercise

--- vscode/tmcdata/mooc-programming-24/part11-08_filter_forbidden/src/filter_forbidden.py
+++ vscode/tmcdata/mooc-programming-24/part11-08_filter_forbidden/src/filter_forbidden.py
@@ -1,1 +1,9 @@
-# WRITE YOUR SOLUTION HERE:
+#!/usr/bin/env python 3
+
+def filter_forbidden(string: str, forbidden: str):
+    return "".join([char for char in string if char not in forbidden])
+
+if __name__ == "__main__":
+    sentence = "Once! upon, a time: there was a python!??!?!"
+    filtered = filter_forbidden(sentence, "!?:,.")
+    print(filtered)
