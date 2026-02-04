47b36963aafd703277804ed241b7c2ee0ec0a93a
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-04 16:28:08 -0600

    Completed the recursive_sum exercise

--- vscode/tmcdata/mooc-programming-24/part11-14_recursive_sum/src/recursive_sum.py
+++ vscode/tmcdata/mooc-programming-24/part11-14_recursive_sum/src/recursive_sum.py
@@ -1,1 +1,15 @@
-# WRITE YOUR SOLUTION HERE:
+#!/usr/bin/env python 3
+
+def recursive_sum(number: int):
+    if number <= 1:
+        return number
+    else:
+        sum = number + recursive_sum(number - 1)
+        return sum
+
+if __name__ == "__main__":
+    result = recursive_sum(3)
+    print(result)
+
+    print(recursive_sum(5))
+    print(recursive_sum(10))
