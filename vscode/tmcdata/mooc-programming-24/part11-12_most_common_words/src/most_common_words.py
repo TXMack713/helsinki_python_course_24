c7720ae7c7d07175b0bf45972d3a414ecfa1a166
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-04 08:19:57 -0600

    Completed the most_common_words exercise

--- vscode/tmcdata/mooc-programming-24/part11-12_most_common_words/src/most_common_words.py
+++ vscode/tmcdata/mooc-programming-24/part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,13 @@
-# WRITE YOUR SOLUTION HERE:
+#!/usr/bin/env python 3
+
+def most_common_words(filename: str, lower_limit: int):
+    all_words = []
+    with open(filename) as file:
+        for line in file:
+            words = line.split(" ")
+            for word in words:
+                all_words.append(word.strip("!,.?\n"))
+    return {word : all_words.count(word) for word in all_words if all_words.count(word) >= lower_limit}
+
+if __name__ == "__main__":
+    print(most_common_words("comprehensions.txt", 3))
