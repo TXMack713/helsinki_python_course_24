c1ca718b3b9e27564b2e182012d879260d59e491
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-06 15:39:31 -0600

    Completed the remaining_stock exercise

--- vscode/tmcdata/mooc-programming-24/part12-01_remaining_stock/src/remaining_stock.py
+++ vscode/tmcdata/mooc-programming-24/part12-01_remaining_stock/src/remaining_stock.py
@@ -1,1 +1,15 @@
-# Write your solution here:
+#!/usr/bin/env python 3
+
+def sort_by_remaining_stock(items: list):
+    products = items
+    def sort_by_stock(items: list):
+        return items[2]
+    
+    return sorted(items, key=sort_by_stock)
+
+if __name__ == "__main__":
+    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22)]
+
+    for product in sort_by_remaining_stock(products):
+        print(f"{product[0]} {product[2]} pcs")
+
