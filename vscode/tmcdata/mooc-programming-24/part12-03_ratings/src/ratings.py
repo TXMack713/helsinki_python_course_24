e189b50929a560f5ac02eef2fe0bc0656f44ff8e
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-06 15:45:04 -0600

    Completed the ratings exercise

--- vscode/tmcdata/mooc-programming-24/part12-03_ratings/src/ratings.py
+++ vscode/tmcdata/mooc-programming-24/part12-03_ratings/src/ratings.py
@@ -1,1 +1,15 @@
-# TEE RATKAISUSI TÄHÄN:
+#!usr/bin/env python 3
+
+def sort_by_ratings(items: list):
+    def ratings_sorter(items: list):
+        return items["rating"]
+
+    return sorted(items, key=ratings_sorter, reverse=True)
+
+if __name__ == "__main__":
+    shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+    print("Rating according to IMDB")
+    for show in sort_by_ratings(shows):
+        print(f"{show['name']} {show['rating']}")
+    
