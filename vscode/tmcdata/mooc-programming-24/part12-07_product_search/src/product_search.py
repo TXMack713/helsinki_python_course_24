652f7df68e5ab291a999f7c7512a45df6e8db019
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-09 13:25:26 -0600

    Completed the product_search exercise

--- vscode/tmcdata/mooc-programming-24/part12-07_product_search/src/product_search.py
+++ vscode/tmcdata/mooc-programming-24/part12-07_product_search/src/product_search.py
@@ -1,1 +1,15 @@
-# Write your solution here
+#!/usr/bin/env python 3
+
+def search(products: list, criterion: callable):
+    return [product for product in products if criterion(product) == True]
+
+def price_under_4_euros(product):
+    # print(f"{product[1]}: {product[1] < 4}")
+    return product[1] < 4
+
+if __name__ == "__main__":
+    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22), ("kale", 0.99, 1)]
+    
+    for product in search(products, price_under_4_euros):
+        print(product)
+
