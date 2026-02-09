bf26dae9282339164f35cdc3338ca090dca4e7ed
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-09 11:14:12 -0600

    Completed the ballplayers exercise

--- vscode/tmcdata/mooc-programming-24/part12-06_ballplayers/src/ballplayers.py
+++ vscode/tmcdata/mooc-programming-24/part12-06_ballplayers/src/ballplayers.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python 3
+
 class BallPlayer:
     def __init__(self, name: str, number: int, goals: int, passes: int, minutes: int):
         self.name = name
@@ -11,4 +13,25 @@ class BallPlayer:
             f'goals={self.goals}, passes={self.passes}, minutes={self.minutes})')
 
 
-# Write your solution here
+def most_goals(items: list):
+    player = max(items, key=lambda item:item.goals)
+    return f"{player.name}"
+
+def most_points(items: list):
+    player = max(items, key=lambda item:item.goals+item.passes)
+    return (player.name, player.number)
+
+def least_minutes(items: list):
+    return min(items, key=lambda item:item.minutes)
+
+if __name__ == "__main__":
+    player1 = BallPlayer("Archie Bonkers", 13, 5, 12, 46)
+    player2 = BallPlayer("Speedy Tickets", 7, 2, 26, 55)
+    player3 = BallPlayer("Cruella De Hill", 9, 1, 32, 26)
+    player4 = BallPlayer("Devilled Tasmanian", 12, 1, 11, 41)
+    player5 = BallPlayer("Donald Quack", 4, 3, 9, 12)
+    
+    team = [player1, player2, player3, player4, player5]
+    print(most_goals(team))
+    print(most_points(team))
+    print(least_minutes(team))
