b9fd82b1eed1fd9e2e376be056f36ad21176e9e5
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-10 17:06:50 -0600

    Completed the credits exercise

--- vscode/tmcdata/mooc-programming-24/part12-13_credits/src/credits.py
+++ vscode/tmcdata/mooc-programming-24/part12-13_credits/src/credits.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python 3
+
 from functools import reduce
 
 class CourseAttempt:
@@ -9,4 +11,39 @@ class CourseAttempt:
     def __str__(self):
         return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
 
-# Write your solution
+def sum_of_all_credits(attempts: list):
+    return reduce(lambda sum, item: sum + item.credits, attempts, 0)
+
+def sum_of_passed_credits(attempts: list):
+    filtered_list = filter(lambda x: x.grade > 0, attempts)
+    return reduce(lambda sum, item: sum + item.credits, filtered_list, 0)
+
+def average(attempts: list):
+    filtered_list = list(filter(lambda x: x.grade >= 1, attempts))
+    return reduce(lambda sum, item: sum + item.grade, filtered_list, 0) / len(filtered_list)
+
+if __name__ == "__main__":
+    attempt = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    print(attempt)
+    print(attempt.course_name)
+    print(attempt.credits)
+    print(attempt.grade)
+
+    print("")
+
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    
+    credit_sum = sum_of_all_credits([s1, s2, s3])
+    print(credit_sum)
+
+    print("")
+
+    credit_sum = sum_of_passed_credits([s1, s2, s3])
+    print(credit_sum)
+
+    print("")
+
+    ag = average([s1, s2, s3])
+    print(ag)
