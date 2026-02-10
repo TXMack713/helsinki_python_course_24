5787d943cf2c6308a4c2556d5ccfd36fa56e934d
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-10 09:02:48 -0600

    Completed the random_words exercise

--- vscode/tmcdata/mooc-programming-24/part12-10_random_words/src/random_words.py
+++ vscode/tmcdata/mooc-programming-24/part12-10_random_words/src/random_words.py
@@ -1,1 +1,11 @@
-# Write your solution here:
+#!usr/bin/env python 3
+
+import random
+
+def word_generator(characters: str, length: int, amount: int):
+    return (characters[i: i + length] for i in range(amount))
+
+if __name__ == "__main__":
+    wordgen = word_generator("abcdefg", 3, 5)
+    for word in wordgen:
+        print(word)
