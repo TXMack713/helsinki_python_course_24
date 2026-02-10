cfb93dc1bd5a8496dcc911f1838b98b1022b5b60
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-10 08:42:07 -0600

    Completed the prime_numbers exercise

--- vscode/tmcdata/mooc-programming-24/part12-09_prime_numbers/src/prime_numbers.py
+++ vscode/tmcdata/mooc-programming-24/part12-09_prime_numbers/src/prime_numbers.py
@@ -1,1 +1,33 @@
-# Write your solution here
+#!/usr/bin/env python 3
+
+def prime_numbers():
+    number = 1
+    divider = 1
+    counter = 0
+    while True:
+        if number % divider == 0 and divider != 1 and divider != number:
+            counter = 0
+            divider = 1
+            number += 1
+        elif number % divider == 0:
+            counter += 1
+            divider += 1
+        elif divider >= number and counter == 2:
+                prime = number
+                divider = 1
+                number += 1
+                counter = 0
+                yield prime
+        elif divider >= number:
+                divider = 1
+                number += 1
+                counter = 0
+        else:
+            divider += 1
+
+ 
+if __name__ == "__main__":
+    numbers = prime_numbers()
+    for i in range(8):
+        print(next(numbers))
+
