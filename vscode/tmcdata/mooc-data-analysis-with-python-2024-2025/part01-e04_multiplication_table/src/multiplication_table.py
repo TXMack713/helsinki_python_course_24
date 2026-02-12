03a955ad19a0005fd37e6016a95345165921fbe4
Anthony <49073356+TXMack713@users.noreply.github.com>
2026-02-12 17:53:39 -0600

    Completed multiplication_table.py

--- vscode/tmcdata/mooc-data-analysis-with-python-2024-2025/part01-e04_multiplication_table/src/multiplication_table.py
+++ vscode/tmcdata/mooc-data-analysis-with-python-2024-2025/part01-e04_multiplication_table/src/multiplication_table.py
@@ -2,7 +2,13 @@
 
 
 def main():
-    pass
+    for i in range(1, 11):
+        line = ""
+        for j in range(1, 11):
+            line += f"{i * j:>2}"
+            line += "\t"
+        line.rstrip()
+        print(f"{line}")
 
 if __name__ == "__main__":
     main()
